64a47a0c1524c514cccde16a440517524ef5097f
yang.zhenzhen <soul11201@gmail.com>
2017-07-18 17:00:10 +0800

    hw4

--- partb-1/hw4.rkt
+++ partb-1/hw4.rkt
@@ -39,6 +39,8 @@
 ;(list-nth-mod (list 1 2 3 4 5) 1 )
 ;(list-nth-mod (list 1 2 3 4 5) 5 )
 ;(list-nth-mod (list 1 2 3 4 5) 6 )
+
+; 4
 (define ones (lambda () (cons 1 ones)))
 
 (define (ziranshu-stream)
@@ -54,5 +56,67 @@
 
 ;(stream-for-n-steps ones 10)
 ;(stream-for-n-steps ziranshu-stream 10)
-  
-  
+
+
+;5
+(define (funny-number-stream)
+  (letrec ([f (lambda (x) (cons (if (= 0 (remainder x 5))
+                                    (- x)
+                                    x)
+                                (lambda () (f ( + x 1)))))])
+    (f 1)))
+
+;(stream-for-n-steps funny-number-stream 20)
+
+
+
+;6
+(define (dan-then-dog)
+  (letrec ([f (lambda (name)
+                (cons name
+                      (lambda () (f (cond [(equal? "dan.jpg" name) "dog.jpg"]
+                                          [#t "dan.jpg"])))))])
+    (f "dan.jpg")))
+
+
+;(stream-for-n-steps dan-then-dog 20)
+
+;7
+
+(define (stream-add-zero s)
+  (letrec ([f (lambda (x)
+             (cons (cons 0 (car x))
+                   (lambda () (f ((cdr x))))))])
+    (f (s))))
+
+
+;(stream-for-n-steps (lambda () (stream-add-zero dan-then-dog)) 20)
+
+
+
+;8
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda (xs ys)
+                (cons (cons (car xs) (car ys))
+                      (lambda () (f (append (cdr xs) (cons (car xs) null)) (append (cdr ys) (cons (car ys) null))))))])
+    (f xs ys)))
+
+
+;(stream-for-n-steps (lambda () (cycle-lists (list 1 2 3) (list "a" "b" "c" "d"))) 20)
+                     
+
+;9
+(define (vector-assoc v vec)
+  (letrec ([f (lambda (n)
+                (if (= n (vector-length vec))
+                    #f
+                    (let ([t (vector-ref vec n)])
+                      (if (pair? t)
+                          (if (equal? v (car t))
+                              t
+                              (f (+ 1 n)))
+                          (f (+ 1 n))))))])
+          (f 0)))
+                    
+;(vector-assoc 1 (vector 0 0 0))
+;(vector-assoc 1 (vector (list 1 2) 0 0))
